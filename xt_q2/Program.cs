eb34f2b3ecc7e79f43061bac008a998252087419
anatoly.kims@gmail.com <anatoly.kims@gmail.com>
2019-08-10 14:25:04 +0400

    Добавьте файлы проекта.

---
+++ xt_q2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xt_q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
