d694995eb5df0ad6979bf28f0139cb18df4af967
AnatolyKim <52208455+AnatolyKim@users.noreply.github.com>
2019-09-03 22:05:38 +0400

    Add files via upload

---
+++ Task_9.4/Task_9.4.1.js
@@ -0,0 +1,19 @@
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+        if (--timer < 0) {
+            timer = 0;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var countdown = 5,
+        display = document.querySelector('#time');
+    startTimer(countdown, display);
+    setTimeout(function(){window.location.href='Task_9.4.2.html'},6500);
+};
